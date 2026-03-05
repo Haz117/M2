86ade0d69cec6ec1be544d8116a866f47ce304bd
Haz117 <hazelalmaraz91@gmail.com>
2026-03-04 19:53:43 -0600

    Fix: Remove stray 's' character causing ReferenceError

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js - Entry point con polyfills
 
 // Suprimir console.log en producción
-if (!__DEV__) {s
+if (!__DEV__) {
 	console.log = () => {};
 	console.warn = () => {};
 	console.debug = () => {};
